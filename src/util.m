4ae124b19a84cd7d6a909f514c1fd8e36e48b39c
Yuriy Vostrikov <mon@unformed.ru>
2012-03-21 09:28:43 +0000

    Fix assert_fail()

--- src/util.m
+++ src/util.m
@@ -272,7 +272,8 @@ void __attribute__ ((noreturn))
 assert_fail(const char *assertion, const char *file, unsigned line, const char *backtrace, const char *function)
 {
 	_say(S_FATAL, file, line, NULL, "%s: assertion %s failed.\n%s", function, assertion, backtrace);
-	close_all_xcpt(0);
+	if (getpid() == master_pid) /* try to close all accept()ing sockets */
+		close_all_xcpt(0);
 	abort();
 }
 
