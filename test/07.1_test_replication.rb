47034231da5c4a44cd2eb2b71e82fbb181537fdb
Yuriy Vostrikov <mon@unformed.ru>
2012-08-17 08:25:20 +0000

    Simplify tests

--- test/07.1_test_replication.rb
+++ test/07.1_test_replication.rb
@@ -1,23 +1,5 @@
 #!/usr/bin/ruby1.9.1
 
 $:.push 'test'
-require 'replication'
-
-class MasterEnvCompat < MasterEnv
-  def config
-    super + <<EOD
-io_compat = 1
-EOD
-  end
-end
-
-class SlaveEnvCompat < SlaveEnv
-  def config
-    super + <<EOD
-io_compat = 1
-EOD
-  end
-end
-
-test MasterEnvCompat, SlaveEnvCompat
-
+$io_compat="io_compat=1"
+require '07_test_replication'
--- test/07_test_replication.rb
+++ test/07_test_replication.rb
@@ -1,7 +1,101 @@
 #!/usr/bin/ruby1.9.1
 
-$:.push 'test'
-require 'replication'
+$:.push 'test/lib'
+require 'standalone_env'
 
-test MasterEnv, SlaveEnv
+class MasterEnv < StandAloneEnv
+  def test_root
+    super + "_master"
+  end
+
+  def config
+    super + <<EOD
+wal_feeder_bind_addr = "ANY:33034"
+#{$io_compat}
+object_space[0].enabled = 1
+object_space[0].index[0].type = "HASH"
+object_space[0].index[0].unique = 1
+object_space[0].index[0].key_field[0].fieldno = 0
+object_space[0].index[0].key_field[0].type = "STR"
+EOD
+  end
+
+  task :setup => ["feeder_init.lua"]
+  file "feeder_init.lua" do
+    f = open("feeder_init.lua", "w")
+    f.write <<-EOD
+      function replication_filter.id_xlog(obj)
+        local row = feeder.crow(obj)
+        print("row lsn:" .. tostring(row.lsn) ..
+              " scn:" .. tostring(row.scn) ..
+              " tag:" .. row.tag ..
+              " cookie:" .. tostring(row.cookie) ..
+              " tm:" .. row.tm)
+
+        if row.tag ~= feeder.tag.wal then
+                return nil
+        end
+        local box_nop = "\01\00\00\00\00\00"
+
+        if row.scn == 2198 or row.scn == 2199 then
+                return box_nop
+        end
+        return nil
+      end
+    EOD
+    f.close
+  end
+end
+
+class SlaveEnv < StandAloneEnv
+  def test_root
+    super + "_slave"
+  end
+
+  def config
+    @primary_port = 33023
+    super + <<EOD
+wal_feeder_addr = "127.0.0.1:33034"
+wal_feeder_filter = "id_xlog"
+#{$io_compat}
+object_space[0].enabled = 1
+object_space[0].index[0].type = "HASH"
+object_space[0].index[0].unique = 1
+object_space[0].index[0].key_field[0].fieldno = 0
+object_space[0].index[0].key_field[0].type = "NUM"
+EOD
+  end
+  def connect_string
+    "0:33023"
+  end
+end
+
+
+MasterEnv.clean do
+  start
+  master = connect
+  master.ping
+
+  100.times do |i|
+    master.insert [i, i + 1, "abc", "def"]
+    master.insert [i, i + 1, "abc", "def"]
+  end
+
+  SlaveEnv.clean do
+    start
+    sleep(0.5)
+    slave = connect
+    slave.select [99]
+
+    Process.kill("STOP", pid)
+    1000.times do |i|
+      master.insert [i, i + 1, "ABC", "DEF"]
+      master.insert [i, i + 1, "ABC", "DEF"]
+    end
+    Process.kill("CONT", pid)
+    sleep(0.5)
+    slave.select [998]
+    slave.select [999]
+  end
+end
 
