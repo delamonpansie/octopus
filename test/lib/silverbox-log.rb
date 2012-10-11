fe179d662b8ecab359326181dbbe093b91391886
Yuriy Vostrikov <mon@unformed.ru>
2012-10-11 16:13:33 +0400

    Use lambda in define_method

--- test/lib/silverbox-log.rb
+++ test/lib/silverbox-log.rb
@@ -7,7 +7,7 @@ class SilverBox
   LOG_OVERRIDE.map(&:to_sym).each do |name|
     orig_name = "#{name}_nolog".to_sym
     alias_method orig_name, name
-    define_method name, do |*args, &block|
+    define_method name, ->(*args, &block) do
       log "# box.#{name}(#{args.map{|arg| arg.inspect}.join(', ')})\n"
       ret_value = self.__send__(orig_name, *args, &block)
       log "#{ret_value.inspect}\n\n"
