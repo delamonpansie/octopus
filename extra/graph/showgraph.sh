98902063c93c6bc78b4b4e38377010dd8e7738fe
Teodor Sigaev <teodor@sigaev.ru>
2012-12-06 11:22:44 +0400

    add framework for online graph

---
+++ extra/graph/showgraph.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+PORT=$1
+
+while (true) do sleep 0.1; echo 'show stat' | nc localhost $PORT | perl parse.pl ; done |\
+	perl ./driveGnuPlotStreams.pl 3 1 100  0 200000  1024x1024 'insert' 'select' 'delete' 0 0 0
+
