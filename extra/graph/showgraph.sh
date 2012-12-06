c1f52c1f95ccd6b97db1dca856d97e1989371d38
Yuriy Vostrikov <mon@unformed.ru>
2012-12-06 16:07:42 +0400

    Fix grapher protability

--- extra/graph/showgraph.sh
+++ extra/graph/showgraph.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -e
 
-PORT=$1
+PORT=${1:-33015}
 
-while (true) do sleep 1; echo 'show stat' | nc localhost $PORT | perl parse.pl ; done |\
-	perl ./driveGnuPlotStreams.pl 3 1 100  0 500000  1024x1024 'insert' 'select' 'delete' 0 0 0
-
+while true; do
+    sleep 1
+    /bin/echo -e 'show stat\nquit' | nc localhost $PORT | perl parse.pl
+done | perl ./driveGnuPlotStreams.pl 3 1 100  0 2000000  1024x1024 'insert' 'select' 'delete' 0 0 0
