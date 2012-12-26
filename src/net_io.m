delamonpansie/octopus

commit 15c747d5e482eca1212ca3215e7cae35b929151a

Speedup net_io

src/net_io.m
@@ -82,6 +82,7 @@ netmsg_unref(struct netmsg *m, int from)
 			luaL_unref(root_L, LUA_REGISTRYINDEX, (uintptr_t)obj[i] >> 1);
 		else
 			object_decr_ref(obj[i]);
+		obj[i] = 0;
 	}
 }
 
@@ -169,13 +170,12 @@ enlarge(struct netmsg **m)
 void
 net_add_iov(struct netmsg **m, const void *buf, size_t len)
 {
-	struct tnt_object **ref = (*m)->ref + (*m)->count;
 	struct iovec *v = (*m)->iov + (*m)->count;
 	v->iov_base = (char *)buf;
 	v->iov_len = len;
 
 	(*m)->head->bytes += len;
-	*ref = NULL;
+	/* *((*m)->ref + (*m)->count) is NULL here. see netmsg_unref() */
 
 #ifdef NET_IO_TIMESTAMPS
 	(*m)->tstamp[(*m)->count] = ev_now();

commit b71c6b778946ca589159c4d1b93b097ad45131bb

Fix bug introduced in commit 15c747d5e

src/net_io.m
@@ -54,6 +54,7 @@ netmsg_alloc(struct netmsg_head *h)
 	struct netmsg *n = slab_cache_alloc(&netmsg_cache);
 	n->count = n->offset = 0;
 	n->head = h;
+	memset(n->ref, 0, sizeof(n->ref));
 
 	TAILQ_INSERT_TAIL(&h->q, n, link);
 	return n;
@@ -126,6 +127,7 @@ netmsg_concat(struct netmsg_head *dst, struct netmsg_head *src)
 			memcpy(tail->ref + tail->count, m->ref, sizeof(m->ref[0]) * m->count);
 			tail->count += m->count;
 
+			memset(m->ref, 0, sizeof(m->ref[0]) * m->count);
 			slab_cache_free(&netmsg_cache, m);
 		} else {
 			m->head = dst;