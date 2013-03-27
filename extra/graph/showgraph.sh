665f1f8aa79c404daf96aeb5abe12ad464f34a88
Yuriy Vostrikov <mon@unformed.ru>
2013-03-27 15:24:18 +0400

    Extract mod_feeder

--- extra/graph/showgraph.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-PORT=${1:-33015}
-
-while true; do
-    sleep 1
-    /bin/echo -e 'show stat\nquit' | nc localhost $PORT | perl parse.pl
-done | perl ./driveGnuPlotStreams.pl 3 1 100  0 2000000  1024x1024 'insert' 'select' 'delete' 0 0 0
--- third_party/qsort_arg.h
+++
@@ -1,8 +0,0 @@
-#ifndef QSORT_ARG_H
-#define QSORT_ARG_H
-
-#include <sys/types.h>
-
-void qsort_arg(void *a, size_t n, size_t es, int (*cmp)(const void *a, const void *b, void *arg), void *arg);
-
-#endif
