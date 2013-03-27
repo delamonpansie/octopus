b1b21ede17233019677a6c7b1702fd8de2955451
Yuriy Vostrikov <mon@unformed.ru>
2013-03-27 15:25:04 +0400

    Drop modules from core

--- mod/box/src-lua/box_prelude.lua
+++
@@ -1,4 +0,0 @@
-require("box")
-require("box.example_proc")
-require("box.object_space_info")
-print("MoonBox initialized.")
--- test/07.1_test_replication.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby1.9.1
-
-$:.push 'test'
-$io_compat="io_compat=1"
-require '07_test_replication'
