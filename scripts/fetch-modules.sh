f4c44d5835d31db1b1ca09f8d96ce117fa359f5e
Yuriy Vostrikov <mon@unformed.ru>
2013-05-17 14:26:11 +0400

    Fetch module respositories as full remote repos

--- scripts/fetch-modules.sh
+++ scripts/fetch-modules.sh
@@ -1,16 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 test -d .git
 mkdir -p mod client
-
-if test -z "`git branch --list mod_*`"; then
-    git remote show | while read remote_name; do
-        git branch --list --remote $remote_name/{mod,client}_* | while read branch_name; do
-            git branch ${branch_name##$remote_name/} $branch_name
-        done
-    done
-fi
+reference=${OCTOPUS_GIT_CLONE_SHARED:+--reference .}
 
 git branch --list mod_* | while read branch_name; do
     if [ ! -e mod/${branch_name##mod_} ]; then
@@ -22,6 +15,18 @@ git branch --list client_* | while read branch_name; do
     if [ ! -e client/${branch_name##client_} ]; then
         git clone -q --branch $branch_name . client/${branch_name##client_}
     fi
+done
+
+git remote show | while read remote_name; do
+    remote_url=$(git remote show -n origin | sed '/Fetch URL:/!d; s/.*Fetch URL:[[:space:]]*//')
+
+    git branch --list --remote $remote_name/{mod,client}_* | while read branch_name; do
+	branch_name=${branch_name#$remote_name/}
+	dir=${branch_name/_/\/}
+	if [ ! -e $dir ]; then
+	    git clone $reference -q --single-branch --branch $branch_name $remote_url $dir
+	fi
+    done
 done
 
 for mod in mod/*; do
