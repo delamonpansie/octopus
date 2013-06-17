e5b06dd76165014e4c0c2c259ce0f411aecaac54
Yuriy Vostrikov <mon@unformed.ru>
2013-06-17 12:31:09 +0400

    Allow git 1.7.1

--- scripts/fetch-modules.sh
+++ scripts/fetch-modules.sh
@@ -5,13 +5,13 @@ test -d .git
 mkdir -p mod client
 reference=${OCTOPUS_GIT_CLONE_SHARED:+--reference .}
 
-git branch --list mod_* | while read branch_name; do
+git branch -a | sed 's/^..//' | grep '^mod_' | while read branch_name; do
     if [ ! -e mod/${branch_name##mod_} ]; then
         git clone -q --branch $branch_name . mod/${branch_name##mod_}
     fi
 done
 
-git branch --list client_* | while read branch_name; do
+git branch -a | sed 's/^..//' | grep '^client_' | while read branch_name; do
     if [ ! -e client/${branch_name##client_} ]; then
         git clone -q --branch $branch_name . client/${branch_name##client_}
     fi
@@ -20,11 +20,11 @@ done
 git remote show | while read remote_name; do
     remote_url=$(git remote show -n origin | sed '/Fetch URL:/!d; s/.*Fetch URL:[[:space:]]*//')
 
-    git branch --list --remote $remote_name/mod_* $remote_name/client_* | while read branch_name; do
+    git branch -a | sed "s/^..//; s/^remotes\///; s/^$remote_name\///;" | grep "^\(mod_\|client_\)" | while read branch_name; do
 	branch_name=${branch_name#$remote_name/}
 	dir=$(echo $branch_name | tr _ /)
 	if [ ! -e $dir ]; then
-	    git clone $reference -q --single-branch --branch $branch_name $remote_url $dir
+	    git clone $reference -q --branch $branch_name $remote_url $dir
 	fi
     done
 done
