delamonpansie/octopus

commit 499ded2901025d2b0fa1c8347bc637139d413317

Binding to specific addr

src/iproto.m
@@ -644,6 +644,35 @@ code
 }
 @end
 
+
+int
+iproto_fixup_addr(struct octopus_cfg *cfg)
+{
+	extern void out_warning(int v, char *format, ...);
+
+	if (strchr(cfg->primary_addr, ':') == NULL && !cfg->primary_port) {
+		out_warning(0, "Option 'primary_port' is not set");
+		return -1;
+	}
+
+	if (net_fixup_addr(&cfg->primary_addr, cfg->primary_port) < 0) {
+		out_warning(0, "Option 'primary_addr' has ':port' and 'primary_port' is set");
+		return -1;
+	}
+	if (net_fixup_addr(&cfg->secondary_addr, cfg->secondary_port) < 0) {
+		out_warning(0, "Option 'secondary_addr' has ':port' and 'secondary_port' is set");
+		return -1;
+	}
+
+	return 0;
+}
+
+static struct tnt_module iproto_mod = {
+	.check_config = iproto_fixup_addr
+};
+
+register_module(iproto_mod);
+
 void __attribute__((constructor))
 iproto_init(void)
 {

src/net_io.m
@@ -839,17 +839,22 @@ retry_bind:
 void
 tcp_server(va_list ap)
 {
-	int port = va_arg(ap, int); /* TODO: report an error if already has server on this port */
+	const char *addr = va_arg(ap, const char *);
 	void (*handler)(int fd, void *data) = va_arg(ap, void (*)(int, void *));
 	void (*on_bind)(int fd) = va_arg(ap, void (*)(int fd));
 	void *data = va_arg(ap, void *);
 
+	struct sockaddr_in saddr;
 	int cfd, fd, one = 1;
-	struct sockaddr_in addr;
 
-	sinany(&addr, port);
-	if ((fd = server_socket(SOCK_STREAM, &addr, 1, on_bind, fiber_sleep)) < 0)
-		exit(EX_OSERR); /* TODO: better error handling */
+	if (!addr)
+		return; /* exit before yield() will prevent fiber creation */
+
+	if (atosin(addr, &saddr) < 0)
+		return;
+
+	if ((fd = server_socket(SOCK_STREAM, &saddr, 1, on_bind, fiber_sleep)) < 0)
+		return;
 
 	ev_io io = { .coro = 1 };
 	ev_io_init(&io, (void *)fiber, fd, EV_READ);
@@ -996,11 +1001,11 @@ accept_client(int fd, void *data)
 }
 
 void
-tcp_service(struct service *service, u16 port, void (*on_bind)(int fd), void (*wakeup_workers)(ev_prepare *))
+tcp_service(struct service *service, const char *addr, void (*on_bind)(int fd), void (*wakeup_workers)(ev_prepare *))
 {
 	memset(service, 0, sizeof(*service));
-	char *name = xmalloc(13);  /* strlen("iproto:xxxxx") */
-	snprintf(name, 13, "tcp:%i", port);
+	char *name = xmalloc(strlen("iproto:") + strlen(addr) + 1);
+	sprintf(name, "tcp:%s", addr);
 
 	TAILQ_INIT(&service->processing);
 	service->pool = palloc_create_pool(name);
@@ -1011,7 +1016,9 @@ tcp_service(struct service *service, u16 port, void (*on_bind)(int fd), void (*w
 
 	service->output_flusher = fiber_create("tcp/output_flusher", conn_flusher);
 	service->input_reader = fiber_create("tcp/input_reader", input_reader);
-	service->acceptor = fiber_create("tcp/acceptor", tcp_server, port, accept_client, on_bind, service);
+	service->acceptor = fiber_create("tcp/acceptor", tcp_server, addr, accept_client, on_bind, service);
+	if (service->acceptor == NULL)
+		panic("unable to start tcp_service `%s'", addr);
 
 	ev_prepare_init(&service->wakeup, (void *)wakeup_workers);
 	ev_prepare_start(&service->wakeup);
@@ -1046,30 +1053,31 @@ atosin(const char *orig, struct sockaddr_in *addr)
 	char *str = strdupa(orig);
 	char *colon = strchr(str, ':');
 
-	if (colon == NULL)
-		return -1;
+	if (colon != NULL) {
+		*colon = 0;
+		port = atoi(colon + 1); /* port is next after ':' */
+	} else {
+		port = atoi(str);
+	}
 
-	*colon = 0;
+	if (port <= 0 || port >= 0xffff) {
+		say_error("bad port in addr: %s", orig);
+		return -1;
+	}
 
 	memset(addr, 0, sizeof(*addr));
 	addr->sin_family = AF_INET;
 
-	if (strcmp(str, "ANY") != 0) {
+	if (colon == NULL || strcmp(str, "ANY") == 0) {
+		addr->sin_addr.s_addr = INADDR_ANY;
+	} else {
 		if (inet_aton(str, &addr->sin_addr) == 0) {
 			say_syserror("inet_aton");
 			return -1;
 		}
-	} else {
-		addr->sin_addr.s_addr = INADDR_ANY;
 	}
 
-	port = atoi(colon + 1); /* port is next after ':' */
-	if (port <= 0 || port >= 0xffff) {
-		say_error("bad port: %s", colon + 1);
-		return -1;
-	}
 	addr->sin_port = htons(port);
-
 	return 0;
 }
 
@@ -1080,6 +1088,31 @@ sintoa(const struct sockaddr_in *addr)
 	snprintf(buf, sizeof(buf), "%s:%i",
 		 inet_ntoa(addr->sin_addr), ntohs(addr->sin_port));
 	return buf;
+}
+
+int
+net_fixup_addr(char **addr, int port)
+{
+	assert(*addr);
+
+	if (port) {
+		if (strchr(*addr, ':')) {
+			return -1;
+		}
+
+		/* len = addr + ':' + 5 digit max + \0 */
+		char *tmp = malloc(strlen(*addr) + 7);
+		sprintf(tmp, "%s:%i", *addr, port);
+		*addr = tmp;
+		return 1;
+	}
+
+	if (port == 0 && strcmp(*addr, "ANY") == 0) {
+		*addr = NULL;
+		return 1;
+	}
+
+	return 0;
 }
 
 static void __attribute__((constructor))

src/octopus.m
@@ -121,10 +121,17 @@ load_cfg(struct octopus_cfg *conf, i32 check_rdonly)
 	if (n_accepted == 0 || n_skipped != 0)
 		return -1;
 
-	if (module(NULL)->check_config)
-		return module(NULL)->check_config(conf);
-	else
-		return 0;
+	if (net_fixup_addr(&conf->admin_addr, conf->admin_port) < 0) {
+		out_warning(0, "Option 'admin_addr' has ':' and 'admin_port' is set");
+		return -1;
+	}
+
+	foreach_module (m) {
+		if (m->check_config)
+			if (m->check_config(conf) < 0)
+				return -1;
+	}
+	return 0;
 }
 
 int
@@ -178,9 +185,13 @@ module(const char *name)
 {
 	if (name == NULL)
 		name = PRIMARY_MOD;
-        for (struct tnt_module *m = modules_head; m != NULL; m = m->next)
-                if (name == NULL || strcmp(name, m->name) == 0)
-                        return m;
+
+	foreach_module (m) {
+		if (!m->name)
+			continue;
+		if (strcmp(name, m->name) == 0)
+			return m;
+	}
         return NULL;
 }
 

cfg/core.cfg_tmpl
@@ -11,6 +11,7 @@ coredump=0.0, ro
 
 # admin port
 # used for admin's connections
+admin_addr="ANY", ro
 admin_port=0, ro
 
 # Log verbosity, possible values: ERROR=1, CRIT=2, WARN=3, INFO=4(default), DEBUG=5

cfg/iproto.cfg_tmpl
@@ -2,9 +2,11 @@
 # IPROTO
 #
 # Primary port (where updates are accepted)
-primary_port=0, ro, required
+primary_addr="ANY", ro
+primary_port=0, ro
 
 # Secondary port (where only selects are accepted)
+secondary_addr="ANY", ro
 secondary_port=0, ro
 
 # warn about requests which take longer to process

doc/example.cfg
@@ -1,5 +1,6 @@
+primary_addr = "127.0.0.1"
 primary_port = 33013
-admin_port = 33015
+admin_addr = "ANY:33015"
 
 # working directory
 # work_dir = "."

include/net_io.h
@@ -171,13 +171,15 @@ void udp_server(va_list ap);
 int server_socket(int type, struct sockaddr_in *src, int nonblock,
 		  void (*on_bind)(int fd), void (*sleep)(ev_tstamp tm));
 
-void tcp_service(struct service *s , u16 port, void (*on_bind)(int fd), void (*wakeup)(ev_prepare *));
+void tcp_service(struct service *s , const char *addr, void (*on_bind)(int fd), void (*wakeup)(ev_prepare *));
 void wakeup_workers(ev_prepare *ev);
 void service_iproto(struct service *s);
 void iproto_wakeup_workers(ev_prepare *ev);
 
 int atosin(const char *orig, struct sockaddr_in *addr);
 const char *sintoa(const struct sockaddr_in *addr);
+int net_fixup_addr(char **addr, int port);
+
 void service_info(struct tbuf *out, struct service *service);
 
 

src/admin.rl
@@ -355,10 +355,13 @@ admin_accept(int fd, void *data __attribute__((unused)))
 int
 admin_init(void)
 {
+	if (!cfg.admin_addr)
+		return 0;
+
 	if (fiber_create("admin/acceptor", tcp_server,
-			 cfg.admin_port, admin_accept, NULL, NULL) == NULL)
+			 cfg.admin_addr, admin_accept, NULL, NULL) == NULL)
 	{
-		say_syserror("can't bind to %d", cfg.admin_port);
+		say_syserror("can't start tcp_server on :`%s'", cfg.admin_addr);
 		return -1;
 	}
 	return 0;

commit c225990f95d4afcbf4db312db555bb0ac53c0c1d

Fix "reload config" bug introduced by 499ded2

src/iproto.m
@@ -655,14 +655,11 @@ iproto_fixup_addr(struct octopus_cfg *cfg)
 		return -1;
 	}
 
-	if (net_fixup_addr(&cfg->primary_addr, cfg->primary_port) < 0) {
-		out_warning(0, "Option 'primary_addr' has ':port' and 'primary_port' is set");
-		return -1;
-	}
-	if (net_fixup_addr(&cfg->secondary_addr, cfg->secondary_port) < 0) {
-		out_warning(0, "Option 'secondary_addr' has ':port' and 'secondary_port' is set");
-		return -1;
-	}
+	if (net_fixup_addr(&cfg->primary_addr, cfg->primary_port) < 0)
+		out_warning(0, "Option 'primary_addr' is overridden by 'primary_port'");
+
+	if (net_fixup_addr(&cfg->secondary_addr, cfg->secondary_port) < 0)
+		out_warning(0, "Option 'secondary_addr' is overridden by 'secondary_port'");
 
 	return 0;
 }

src/net_io.m
@@ -1097,15 +1097,20 @@ net_fixup_addr(char **addr, int port)
 	assert(*addr);
 
 	if (port) {
-		if (strchr(*addr, ':')) {
-			return -1;
+		int ret = 1;
+		char *c = strchr(*addr, ':');
+		if (c)  {
+			if (atoi(c + 1) == port)
+				return 0;
+			*c = 0;
+			ret = -1;
 		}
 
 		/* len = addr + ':' + 5 digit max + \0 */
 		char *tmp = malloc(strlen(*addr) + 7);
 		sprintf(tmp, "%s:%i", *addr, port);
 		*addr = tmp;
-		return 1;
+		return ret;
 	}
 
 	if (port == 0 && strcmp(*addr, "ANY") == 0) {

src/octopus.m
@@ -121,10 +121,8 @@ load_cfg(struct octopus_cfg *conf, i32 check_rdonly)
 	if (n_accepted == 0 || n_skipped != 0)
 		return -1;
 
-	if (net_fixup_addr(&conf->admin_addr, conf->admin_port) < 0) {
-		out_warning(0, "Option 'admin_addr' has ':' and 'admin_port' is set");
-		return -1;
-	}
+	if (net_fixup_addr(&conf->admin_addr, conf->admin_port) < 0)
+		out_warning(0, "Option 'admin_addr' is overridden by 'admin_port'");
 
 	foreach_module (m) {
 		if (m->check_config)
@@ -177,6 +175,9 @@ reload_cfg(void)
 			m->reload_config(&cfg, &new_cfg2);
 	destroy_octopus_cfg(&cfg);
 	cfg = new_cfg2;
+
+	if (cfg_err_len)
+		say_warn("config warnings: %.*s", cfg_err_len, cfg_err);
 	return 0;
 }
 
@@ -752,6 +753,9 @@ octopus(int argc, char **argv)
 		say_logger_init(cfg.logger_nonblock);
 		booting = false;
 	}
+
+	if (cfg_err_len)
+		say_warn("config warnings: %.*s", cfg_err_len, cfg_err);
 
 #ifdef STORAGE
 	if (gopt(opt, 'i')) {