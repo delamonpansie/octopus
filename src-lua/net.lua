87966ef18457fa00c04822a6268c132260c9ee6a
Yuriy Vostrikov <mon@unformed.ru>
2013-08-16 15:27:22 +0400

    Drop hardcoded cdefs

--- src-lua/net.lua
+++ src-lua/net.lua
@@ -3,12 +3,10 @@ local bit = bit
 local ffi = require("ffi")
 
 local print = print
-local type = type
 local assert = assert
-local format = string.format
-local debug = debug
 local error = error
 local tonumber = tonumber
+local require = require
 
 module(...)
 
@@ -161,107 +159,8 @@ extern uint32_t htonl(uint32_t hostlong);
 extern uint16_t htons(uint16_t hostshort);
 ]]
 
-ffi.cdef[[
-typedef double ev_tstamp;
-
-typedef struct ev_io {
- int active;
- int pending;
- int priority;
- void *data;
- char coro;
- const char *cb_src;
- void (*cb)(struct ev_io *w, int revents);
- struct ev_watcher_list *next;
- int fd;
- int events;
-} ev_io;
-
-typedef struct ev_timer {
- int active;
- int pending;
- int priority;
- void *data;
- char coro;
- const char *cb_src;
- void (*cb)(struct ev_timer *w, int revents);
- ev_tstamp at;
- ev_tstamp repeat;
-} ev_timer;
-]]
-
-ffi.cdef[[
-struct iproto_retcode {
-	uint32_t msg_code;
-	uint32_t data_len;
-	uint32_t sync;
-	uint32_t ret_code;
-};
-
-struct iproto {
-	uint32_t msg_code;
-	uint32_t data_len;
-	uint32_t sync;
-};
-]]
-
-ffi.cdef[[
-
-struct netmsg_head {
- struct netmsg_tailq { struct netmsg *tqh_first; struct netmsg **tqh_last; } q;
- struct palloc_pool *pool;
- size_t bytes;
-};
-
-struct netmsg {
- struct netmsg_head *head;
- unsigned offset, count;
- struct { struct netmsg *tqe_next; struct netmsg **tqe_prev; } link;
- struct iovec dummy;
- struct iovec iov[64];
- uintptr_t ref[64];
-};
-
-enum conn_memory_ownership {
- MO_MALLOC = 0x01,
- MO_STATIC = 0x02,
- MO_SLAB = 0x03,
- MO_MY_OWN_POOL = 0x10
-};
-
-struct conn {
- struct palloc_pool *pool;
- struct tbuf *rbuf;
- int fd, ref;
- struct netmsg_head out_messages;
- enum conn_memory_ownership memory_ownership;
- enum { CLOSED, IN_CONNECT, CONNECTED } state;
- struct link { struct conn *le_next; struct conn **le_prev; } link;
- struct processing_link { struct conn *tqe_next; struct conn **tqe_prev; } processing_link;
- ev_io in, out;
- struct service *service;
- char peer_name[22];
- ev_timer timer;
-};
-
-struct conn_wrap {
- struct conn *ptr;
-};
-
-void netmsg_release(struct netmsg *m);
-void netmsg_rewind(struct netmsg_head *h, struct netmsg_mark *mark);
-void netmsg_getmark(struct netmsg_head *h, struct netmsg_mark *mark);
-
-void net_add_iov(struct netmsg_head *o, const void *buf, size_t len);
-struct iovec *net_reserve_iov(struct netmsg_head *o);
-void net_add_iov_dup(struct netmsg_head *o, const void *buf, size_t len);
-void net_add_ref_iov(struct netmsg_head *o, uintptr_t ref, const void *buf, size_t len);
-void net_add_obj_iov(struct netmsg_head *o, struct tnt_object *obj, const void *buf, size_t len);
-
-int conn_unref(struct conn *c);
-
-extern int atosin(const char *orig, struct sockaddr *addr);
-]]
+require 'net_cdef'
+ffi.cdef 'struct conn_wrap { struct conn *ptr; };'
 
 local C = ffi.C
 
@@ -306,11 +205,11 @@ end
 
 
 -- gc ref's
-local ref_registry = {[0]=nil}
+local ref_registry = {[0]=0}
 
 function ref(obj)
    local ref = ref_registry[0]
-   if ref then
+   if ref > 0 then
       ref_registry[0] = ref_registry[ref]
    else
       ref = #ref_registry + 1
