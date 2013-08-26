delamonpansie/octopus

commit 0dacbb84669b619b00df1ca0eb7f8275388fbce3

Rewind connection->out_messages on lua error

moonbox.m
@@ -302,6 +302,9 @@ box_dispach_lua(struct conn *c, struct iproto *request)
 		iproto_raise_fmt(ERR_CODE_ILLEGAL_PARAMS, "no such proc: %.*s", flen, fname);
 	}
 
+	struct netmsg_mark mark;
+	netmsg_getmark(&c->out_messages, &mark);
+
 	lua_pushlightuserdata(L, c);
 	lua_pushlightuserdata(L, request);
 
@@ -310,6 +313,7 @@ box_dispach_lua(struct conn *c, struct iproto *request)
 
 	/* FIXME: switch to native exceptions ? */
 	if (lua_pcall(L, 2 + nargs, 0, 0)) {
+		netmsg_rewind(&c->out_messages, &mark);
 		IProtoError *err = [IProtoError palloc];
 		[err init_code:ERR_CODE_ILLEGAL_PARAMS
 			  line:__LINE__

commit d342e79bbed0ab12ccaaf20a39b12aec7208937a

Fix bug introduced in 0dacbb84
It's an error to try rewind conn the way 0dacbb84 did: lua proc may block in core
and conn->out_messages will be populated by concurent op's running on same connection.
Such rewind will effectively drop any output they produced.

moonbox.m
@@ -302,9 +302,6 @@ box_dispach_lua(struct conn *c, struct iproto *request)
 		iproto_raise_fmt(ERR_CODE_ILLEGAL_PARAMS, "no such proc: %.*s", flen, fname);
 	}
 
-	struct netmsg_mark mark;
-	netmsg_getmark(&c->out_messages, &mark);
-
 	lua_pushlightuserdata(L, c);
 	lua_pushlightuserdata(L, request);
 
@@ -313,7 +310,6 @@ box_dispach_lua(struct conn *c, struct iproto *request)
 
 	/* FIXME: switch to native exceptions ? */
 	if (lua_pcall(L, 2 + nargs, 0, 0)) {
-		netmsg_rewind(&c->out_messages, &mark);
 		IProtoError *err = [IProtoError palloc];
 		[err init_code:ERR_CODE_ILLEGAL_PARAMS
 			  line:__LINE__

src-lua/box.lua
@@ -147,35 +147,44 @@ function wrap(proc_body)
                 return nil
         end
 
+	local function append(out, request, ret_code, result)
+	   local header = out:add_iov_iproto_header(request)
+	   local bytes = out:bytes()
+
+	   if type(result) == "table" then
+	      out:add_iov_string(tou32(#result))
+
+	      for k, v in pairs(result) do
+		 if type(v) == "string" then
+		    out:add_iov_string(v)
+		 elseif type(v) == "userdata" then
+		    local obj = tnt_object_ref(v)[0]
+		    local tuple = ffi.cast(tuple_t, obj.data)
+		    ffi.C.object_incr_ref(obj)
+		    out:add_iov_ref(obj.data, tuple.bsize + 8, ffi.cast('uintptr_t', obj))
+		 else
+		    error("unexpected type of result: " .. type(v))
+		 end
+	      end
+	   elseif type(result) == "number" then
+	      out:add_iov_string(tou32(result))
+	   else
+	      error("unexpected type of result: " .. type(result))
+	   end
+
+	   header.data_len = header.data_len + out:bytes() - bytes
+	   header.ret_code = ret_code
+	end
+
         local function proc(out, request, ...)
-                local retcode, result = proc_body(...)
-		local out = net.conn(out)
-		local header = out:add_iov_iproto_header(request)
-		local bytes = out:bytes()
+	   -- proc_body may fail and may block in core
+	   -- it's unsafe to modify 'struct conn' while blocking in core,
+	   -- because of possible concurent updates
+	   -- so, append to conn in atomically via out:pcall
 
-                if type(result) == "table" then
-                        out:add_iov_string(tou32(#result))
-
-                        for k, v in pairs(result) do
-			   if type(v) == "string" then
-			      out:add_iov_string(v)
-			   elseif type(v) == "userdata" then
-			      local obj = tnt_object_ref(v)[0]
-			      local tuple = ffi.cast(tuple_t, obj.data)
-			      ffi.C.object_incr_ref(obj)
-			      out:add_iov_ref(obj.data, tuple.bsize + 8, ffi.cast('uintptr_t', obj))
-			   else
-			      error("unexpected type of result: " .. type(v))
-			   end
-			end
-                elseif type(result) == "number" then
-                        out:add_iov_string(tou32(result))
-                else
-                        error("unexpected type of result: " .. type(result))
-                end
-
-		header.data_len = header.data_len + out:bytes() - bytes
-		header.ret_code = retcode
+	   local ret_code, result = proc_body(...)
+	   local out = net.conn(out)
+	   out:pcall(append, request, ret_code, result)
         end
 
         return proc