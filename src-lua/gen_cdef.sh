ca1462d160c366143077bc657d48df960a3c73b3
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2013-11-28 14:39:18 +0400

    add iproto_0 and iproto_retcode_0

--- src-lua/gen_cdef.sh
+++ src-lua/gen_cdef.sh
@@ -12,6 +12,7 @@ struct palloc_pool;
 extern struct fiber *fiber;
 `$CPP -P $CPPFLAGS $srcdir/include/octopus_ev.h | $SED -n '/^typedef [a-z]\+ ev_tstamp/p; /typedef struct ev_\(io\|timer\)/,/^}/p;'`
 `$CPP -P $CPPFLAGS $srcdir/include/iproto_def.h | $SED -n '/^struct iproto\(_retcode\)\? \+{/,/^}/{s/\[\]/[?]/;p;}'`
+`$CPP -P $CPPFLAGS $srcdir/include/iproto_def.h | $SED -n '/^struct iproto\(_retcode\)\? \+{/,/^}/{s/\(iproto\w*\)/\1_0/;/\[\]/ d;p;}'`
 `$CPP -P $CPPFLAGS $srcdir/include/net_io.h | $SED -n '/^struct netmsg\(_mark\|_head\|_tailq\)\? \+{/,/^}/p; /^enum conn_memory_ownership \+{/,/^}/p; /^struct conn \+{/,/^}/p'`
 `$CPP -P $CPPFLAGS -DLUA_DEF='@@@' $srcdir/include/net_io.h | $SED -n '/@@@/!d; s/@@@//; p'`
 `$CPP -P $CPPFLAGS $srcdir/include/tbuf.h | $SED -n '/^struct tbuf \+{/,/^}/{s/end/stop/;s/void/u8/;p}'`
