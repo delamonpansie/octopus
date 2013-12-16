1dc73d76e86af50e13f58fb80e0ffc6854362968
Yuriy Vostrikov <mon@unformed.ru>
2013-12-16 11:55:02 +0400

    Rewrite graphite.lua

--- extra/graphite.lua
+++ extra/graphite.lua
@@ -1,29 +1,21 @@
-local addr = graphite_addr or '127.0.0.1:2003'
-
---
 --
 -- this script will periodically send stats to graphite
--- to enable: put it to cfg.workdir, add reloadfile('graphite.lua')
--- to init.lua and edit graphite_addr abowe
---
+-- to enable: put it to cfg.workdir, and add
+-- require('graphite.lua').start('1.2.3.4:2003') to init.lua
 --
 
 local ffi = require 'ffi'
 local fiber = require 'fiber'
 require 'net' -- for ffi.cdef
+local pcall, type, pairs, collectgarbage = pcall, type, pairs, collectgarbage
+local print, tostring, tonumber = print, tostring, tonumber
+local string, table = string, table
+local stat = stat
 
-if not graphite_loaded then
-   graphite_loaded = true
-   local loop = function ()
-      while true do
-	 fiber.sleep(60)
-	 if type(graphite_sender) == 'function' then
-	    graphite_sender()
-	 end
-      end
-   end
-   fiber.create(loop)
-   ffi.cdef[[
+module(...)
+
+ffi.cdef[[
+const char *octopus_version(void);
 extern int gethostname(char *name, size_t len);
 extern char *custom_proc_title;
 extern char *primary_addr;
@@ -31,62 +23,110 @@ extern char *primary_addr;
 typedef long int time_t;
 time_t time(time_t *t);
 ]]
+
+local function gethostname()
+    local buf = ffi.new('char[?]', 64)
+    local result = "unknown"
+    if ffi.C.gethostname(buf, ffi.sizeof(buf)) ~= -1 then
+        result = ffi.string(buf)
+    end
+    if ffi.C.primary_addr ~= nil then
+        result = result .. ":" .. ffi.string(ffi.C.primary_addr)
+    end
+    if ffi.C.custom_proc_title ~= nil then
+        local proctitle = ffi.string(ffi.C.custom_proc_title)
+        if #proctitle > 0 then
+            result = result .. proctitle
+        end
+    end
+    return string.gsub(result, '[. ()]+', '_')
 end
 
-local function gethostname() 
-   local buf = ffi.new('char[?]', 64)
-   local result = "unknown"
-   if ffi.C.gethostname(buf, ffi.sizeof(buf)) ~= -1 then
-      result = ffi.string(buf)
-   end
-   if ffi.C.primary_addr ~= nil then
-      result = result .. ":" .. ffi.string(ffi.C.primary_addr)
-   end
-   if ffi.C.custom_proc_title ~= nil then
-      local proctitle = ffi.string(ffi.C.custom_proc_title)
-      if #proctitle > 0 then
-	 result = result .. proctitle
-      end
-   end
-   return string.gsub(result, '[. ()]+', '_')
+
+local callback = {}
+
+local function makemsg()
+    local hostname = gethostname()
+    local time = tostring(tonumber(ffi.C.time(nil)))
+    local msg = {}
+
+    local head = ("my.octopus.%s."):format(gethostname())
+    local tail = (" %i\n"):format(tonumber(ffi.C.time(nil)))
+
+    for n, cb in pairs(callback) do
+        local ok, t = pcall(cb)
+        if ok and type(t) == 'table' then
+            local type = n .. "."
+            for k, v in pairs(t) do
+                table.insert(msg, head)
+                table.insert(msg, type)
+                table.insert(msg, k)
+                table.insert(msg, " ")
+                table.insert(msg, v)
+                table.insert(msg, tail)
+            end
+        else
+            print("error in graphite callback '" .. n .. "'")
+        end
+    end
+    if #msg == 0 then
+        return nil
+    end
+    return table.concat(msg)
 end
 
-local function graphite()
-   if type(stat) ~= 'table' or type(stat.records) ~= 'table' then
-      -- stat module either not loaded or disabled
-      return nil
-   end
-   local hostname = gethostname()
-   local time = tostring(tonumber(ffi.C.time(nil)))
-   local msg = {}
-   for k, v in pairs(stat.records[0]) do
-      table.insert(msg, "my.octopus.")
-      table.insert(msg, hostname)
-      table.insert(msg, ".")
-      table.insert(msg, k)
-      table.insert(msg, " ")
-      table.insert(msg, v)
-      table.insert(msg, " ")
-      table.insert(msg, time)
-      table.insert(msg, "\n")
-   end
-   return table.concat(msg)
-end
 
-local sockaddr = ffi.new('struct sockaddr_in')
+local addrvalid
+local sockaddr_in = ffi.new('struct sockaddr_in')
+local sockaddr = ffi.cast('struct sockaddr *', sockaddr_in)
 local sock = ffi.C.socket(ffi.C.PF_INET, ffi.C.SOCK_DGRAM, 0)
 
-function graphite_sender ()
-    local msg = graphite()
-    if (graphite_addr ~= addr) then
-       ffi.C.atosin(addr, sockaddr)
-       addr = graphite_addr
-       print('Graphite export to ' .. addr)
-    end
-    if msg then
-       ffi.C.sendto(sock, msg, #msg, 0,
-		    ffi.cast('struct sockaddr *', sockaddr), ffi.sizeof(sockaddr))
+function add_cb(name, f)
+    callback[name] = f
+end
+
+function start(addr)
+    local rc = ffi.C.atosin(addr, sockaddr_in)
+    if rc >= 0 then
+        addrvalid = true
+        print('Graphite export to ' .. addr)
+    else
+        addrvalid = false
     end
 end
 
-print('Graphite export to ' .. addr)
+local function worker_loop()
+    fiber.sleep(60)
+    if addrvalid then
+        local msg = makemsg()
+        if msg then
+            ffi.C.sendto(sock, msg, #msg, 0, sockaddr, ffi.sizeof(sockaddr_in))
+        end
+    end
+    worker_loop()
+end
+
+if graphite_addr then
+    start(graphite_addr)
+end
+fiber.create(worker_loop)
+
+-- common stats
+local function statcb()
+    local stat_ready = pcall(function () pairs(stat.records[0]) end)
+    if not stat_ready then
+        -- stat module either not loaded or disabled
+        return {}
+    end
+    return stat.records[0]
+end
+local function gccb()
+    return {["count"] = collectgarbage("count")}
+end
+
+add_cb("stat", statcb)
+add_cb("gc", gccb)
+local version = tonumber(tostring(ffi.C.octopus_version):match('bundle:([0-9]+)'))
+if version then
+    add_cb("version", function () return {["version"] = version} end)
+end
