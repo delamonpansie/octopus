e01b92bc978eb3a78a44841bab9a5f168f38bcca
Yuriy Vostrikov <mon@unformed.ru>
2014-02-07 17:20:21 +0400

    Update copyright notice

--- moonbox.h
+++ moonbox.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2010, 2012 Mail.RU
- * Copyright (C) 2010, 2012 Yuriy Vostrikov
+ * Copyright (C) 2010, 2012, 2013 Mail.RU
+ * Copyright (C) 2010, 2012, 2013 Yuriy Vostrikov
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
