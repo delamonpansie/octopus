59e6dc91bc299ac5a0c0db089c7e83853e8bec24
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2014-03-05 19:26:59 +0400

    no reload_files_queue

--- src-lua/reloadfile.lua
+++ src-lua/reloadfile.lua
@@ -1,7 +1,6 @@
 local reload_files = {}
 local reload_modules = {}
 local reload_lock = {"it is reload loop lock"}
-local reload_files_queue = nil
 
 local function print_warn(name, msg)
     print(string.format("reloadfile(\"%s\"): %s", name, msg))
@@ -74,19 +73,6 @@ local function reload_fullpath_loader(name)
 end
 table.insert(package.loaders, 1, reload_fullpath_loader)
 
-local function push_to_queue(name)
-    if not reload_files_queue then
-        reload_files_queue = {}
-    end
-    if reload_files_queue[name] == nil then
-        reload_files_queue[name] = true
-        table.insert(reload_files_queue, name)
-    end
-    if #fiber._locks[reload_lock] == 1 then
-        fiber._unlock(reload_lock)
-    end
-end
-
 local function check_reload(name)
     local stat = reload_files[name]
     if not stat.filename and not fix_reload_filename(name) then
@@ -116,17 +102,22 @@ local function check_reload(name)
     end
 end
 
+local loop_unlocked = false
+local function unlock_reload_loop()
+    if #fiber._locks[reload_lock] == 1 then
+        fiber._unlock(reload_lock)
+    end
+    loop_unlocked = true
+end
 
 local function reload_loop()
     while true do
-        fiber._lock(reload_lock)
-        while reload_files_queue do
-            local rfq = reload_files_queue
-            reload_files_queue = nil
-            for _, name in ipairs(rfq) do
-                check_reload(name)
-            end
-            fiber.sleep(1)
+        if not loop_unlocked then
+            fiber._lock(reload_lock)
+        end
+        loop_unlocked = false
+        for _, name in ipairs(reload_files) do
+            check_reload(name)
         end
     end
 end
@@ -134,9 +125,7 @@ end
 local function reload_queue_pusher()
     while true do
         fiber.sleep(1)
-        for _, name in ipairs(reload_files) do
-            push_to_queue(name)
-        end
+        unlock_reload_loop()
     end
 end
 
@@ -147,6 +136,6 @@ fiber.create(reload_queue_pusher)
 function reloadfile(name)
     assertarg(name, 'string', 1)
     local stat = register_reload(name)
-    push_to_queue(name)
+    unlock_reload_loop()
 end
 return reloadfile
