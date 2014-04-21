95cfd9e4dd80f2b2f1b8c26df54122e61a4517d8
Yuriy Vostrikov <mon@unformed.ru>
2014-04-21 12:22:44 +0400

    Update copyright

--- salloc.c
+++ salloc.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2010, 2011 Mail.RU
- * Copyright (C) 2010, 2011 Yuriy Vostrikov
+ * Copyright (C) 2010, 2011, 2013, 2014 Mail.RU
+ * Copyright (C) 2010, 2011, 2013, 2014 Yuriy Vostrikov
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
