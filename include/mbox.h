18161609a1430d1b0d5fdee4a2b9892f692037a4
Yuriy Vostrikov <mon@unformed.ru>
2014-06-05 15:26:33 +0400

    More generic mbox implementation

--- include/mbox.h
+++ include/mbox.h
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2010, 2011 Mail.RU
+ * Copyright (C) 2010, 2011, 2014 Mail.RU
  * Copyright (C) 2011 Teodor Sigaev
+ * Copyright (C) 2014 Yuriy Vostrikov
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,103 +34,73 @@
 #include <util.h>
 #include <fiber.h>
 
-#define MBOX_MAGICK_COOKIE	((void*)(uintptr_t)0xBADC0DEF)
-
 struct mbox_consumer {
 	struct	fiber			*fiber;
-	TAILQ_ENTRY(mbox_consumer)	conslink;
+	LIST_ENTRY(mbox_consumer)	conslink;
 };
 
-struct mbox_msg {
-	void				*msg;
-	STAILQ_ENTRY(mbox_msg)		msglink;
-};
-
-struct mbox {
-	STAILQ_HEAD(mbox_msg_list, mbox_msg)		msg_list;
-	u32						msg_list_len;
-	TAILQ_HEAD(mbox_consumer_list, mbox_consumer)	consumer_list;
-};
-
-static inline void
-mbox_init(struct mbox *mbox) {
-	STAILQ_INIT(&mbox->msg_list);
-	TAILQ_INIT(&mbox->consumer_list);
-	mbox->msg_list_len = 0;
+#define MBOX(name, type)						\
+struct name {								\
+	LIST_HEAD(, mbox_consumer) consumer_list;	\
+	STAILQ_HEAD(, type) msg_list;			\
+	int msg_count;							\
 }
 
-static inline void
-mbox_put(struct mbox *mbox, struct mbox_msg *msg) {
-	struct mbox_consumer	*consumer;
+#define MBOX_INITIALIZER(name)						\
+	{ LIST_HEAD_INITIALIZER(name.consumer_list),			\
+	  STAILQ_HEAD_INITIALIZER(name.msg_list),			\
+	  0 }
 
-	STAILQ_INSERT_TAIL(&mbox->msg_list, msg, msglink);
-	mbox->msg_list_len++;
+#define mbox_init(mbox) ({						\
+	LIST_INIT(&(mbox)->consumer_list);				\
+	STAILQ_INIT(&(mbox)->msg_list);					\
+	(mbox)->msg_count = 0;						\
+})
 
-	TAILQ_FOREACH(consumer, &mbox->consumer_list, conslink)
-	fiber_wake(consumer->fiber, MBOX_MAGICK_COOKIE);
-}
+#define mbox_put(mbox, msg, link) ({					\
+	STAILQ_INSERT_TAIL(&(mbox)->msg_list, msg, link); 		\
+	(mbox)->msg_count++;						\
+	struct mbox_consumer *consumer;					\
+	LIST_FOREACH(consumer, &(mbox)->consumer_list, conslink)	\
+		fiber_wake(consumer->fiber, msg);			\
+})
 
-static inline void *
-mbox_get(struct mbox *mbox) {
-	void *msg = NULL;
+#define mbox_msgtype(mbox) typeof((mbox)->msg_list.stqh_first)
 
-	if (mbox->msg_list_len > 0) {
-		struct mbox_msg *mbox_msg = STAILQ_FIRST(&mbox->msg_list);
+#define mbox_get(mbox, link) ({						\
+	mbox_msgtype(mbox) msg = STAILQ_FIRST(&(mbox)->msg_list);	\
+	if (msg) {							\
+		STAILQ_REMOVE_HEAD(&(mbox)->msg_list, link);		\
+		(mbox)->msg_count--;					\
+	}								\
+	msg;								\
+})
 
-		assert(mbox_msg != NULL);
+#define mbox_wait(mbox) ({						\
+	struct mbox_consumer consumer = { .fiber = fiber }; 		\
+	LIST_INSERT_HEAD(&(mbox)->consumer_list, &consumer, conslink);	\
+	mbox_msgtype(mbox) msg = yield();				\
+	LIST_REMOVE(&consumer, conslink);	\
+	msg;								\
+})
 
-		STAILQ_REMOVE_HEAD(&mbox->msg_list, msglink);
-		mbox->msg_list_len--;
-
-		msg = mbox_msg->msg;
-	} else {
-		assert(STAILQ_FIRST(&mbox->msg_list) == NULL);
-	}
-
-	return msg;
-}
-
-static inline void
-mbox_wait(struct mbox *mbox) {
-	struct mbox_consumer	mbox_consumer = { .fiber = fiber };
-
-	TAILQ_INSERT_TAIL(&mbox->consumer_list, &mbox_consumer,  conslink);
-	while(mbox->msg_list_len == 0) {
-		void *r = yield();
-
-		assert(r == MBOX_MAGICK_COOKIE);
-		(void)r; /* keep compiler quiet */
-	}
-	TAILQ_REMOVE(&mbox->consumer_list, &mbox_consumer,  conslink);
-}
-
-static inline bool
-mbox_timedwait(struct mbox *mbox, ev_tstamp delay) {
-	struct mbox_consumer	mbox_consumer = { .fiber = fiber };
-	ev_timer 		w = { .coro = 1 };
-	bool			res = true;
-
-	ev_timer_init(&w, (void *)fiber, delay, 0.);
-
-	ev_timer_start(&w);
-	TAILQ_INSERT_TAIL(&mbox->consumer_list, &mbox_consumer,  conslink);
-
-	while(mbox->msg_list_len == 0) {
-		void *r = yield();
-
-		if (r == (void*)&w) {
-			/* timeout */
-			res = false;
-			break;
-		} else {
-			assert(r == MBOX_MAGICK_COOKIE);
-		}
-	}
-
-	TAILQ_REMOVE(&mbox->consumer_list, &mbox_consumer,  conslink);
-	ev_timer_stop(&w);
-
-	return res;
-}
+#define mbox_timedwait(mbox, count, delay) ({				\
+	ev_timer w = { .coro = 1 };					\
+	if (delay) {							\
+		ev_timer_init(&w, (void *)fiber, delay, 0);		\
+		ev_timer_start(&w);					\
+	}								\
+	mbox_msgtype(mbox) msg;						\
+	while ((mbox)->msg_count < count) {				\
+		msg = mbox_wait(mbox);					\
+		if (msg == (void *)&w) { /* timeout */			\
+			msg = NULL;					\
+			break;						\
+		}							\
+	}								\
+	if (delay)							\
+		ev_timer_stop(&w);					\
+	msg;								\
+})
 
 #endif
