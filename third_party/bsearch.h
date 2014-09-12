f70d5eb64f77fa28766a1586971c728d47f9f928
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2014-09-12 19:47:40 +0400

    bsearch.h: more fixes and cosmetic

--- third_party/bsearch.h
+++ third_party/bsearch.h
@@ -29,106 +29,118 @@
 /*
 	BSEARCH_STRUCT(long) bs;
 	BSEARCH_INIT_SEARCH_LAST(&bs, count);
-	while (BSEARCH_NEXT_SEARCH_LAST(&bs)) {
-		BSEARCH_STEP_SEARCH_LAST_LE(&bs, cmp(key, objs[bs.mid]));
+	while (BSEARCH_LAST_NOT_FOUND(&bs)) {
+		BSEARCH_STEP_TO_LAST_LE(&bs, cmp(key, objs[bs.mid]));
 	}
-	print("equal %d", bs.equal);
+	print("index of last less or equal %d equal %d", bs.mid, bs.equal);
 
 	BSEARCH_INIT_SEARCH_FIRST(&bs, count);
-	while (BSEARCH_NEXT_SEARCH_FIRST(&bs)) {
-		BSEARCH_STEP_SEARCH_FIRST_GT(&bs, cmp(key, objs[bs.mid]));
+	while (BSEARCH_FIRST_NOT_FOUND(&bs)) {
+		BSEARCH_STEP_TO_FIRST_GT(&bs, cmp(key, objs[bs.mid]));
 	}
+	print("index of first greater %d", bs.mid);
 
 	BSEARCH_INIT_SEARCH_EQUAL(&bs, count);
-	while (BSEARCH_NEXT_SEARCH_EQUAL(&bs)) {
-		BSEARCH_STEP_SEARCH_EQUAL_GT(&bs, cmp(key, objs[bs.mid]));
+	while (BSEARCH_EQUAL_NOT_FOUND(&bs)) {
+		BSEARCH_STEP_TO_EQUAL(&bs, cmp(key, objs[bs.mid]));
 	}
-	print("found %d", bs.equal);
+	print("index of some equal or first greater %d equal %d", bs.mid, bs.equal);
 */
-#define BSEARCH_STRUCT(index_t) struct { index_t low, high, mid; bool equal; }
-#define BSEARCH_STRUCT_NAME(index_t, name) struct name { index_t low, high, mid; bool equal; }
+#define BSEARCH_STRUCT(index_t) struct { index_t low, high, mid; int equal; }
+#define BSEARCH_STRUCT_NAME(index_t, name) struct name { index_t low, high, mid; int equal; }
 
 #define BSEARCH_INIT_SEARCH_LAST(bs_, count) do { \
 	__typeof__(bs_) _bs_ = (bs_); _bs_->low = _bs_->mid = -1; _bs_->high = (count) - 1; _bs_->equal = 0; \
+	assert(_bs_->low <= _bs_->high); \
 } while(0)
 
-#define BSEARCH_NEXT_SEARCH_LAST(bs_) ({ \
+#define BSEARCH_LAST_NOT_FOUND(bs_) ({ \
 	__typeof__(bs_) _bs_ = (bs_); \
 	_bs_->mid = _bs_->low + ((_bs_->high - _bs_->low + 1) / 2); \
 	_bs_->low < _bs_->high; \
 })
 
-#define BSEARCH_STEP_SEARCH_LAST_LE(bs_, cmp_) do { \
+#define BSEARCH_STEP_TO_LAST_LE(bs_, cmp_) do { \
 	__typeof__(bs_) _bs_ = (bs_); \
-	__typeof__(cmp_) cmp = (cmp); \
-	if (cmp >= 0) { _bs_->low = _bs_->mid; _bs_->equal = cmp == 0; } \
+	__typeof__(cmp_) _cmp_ = (cmp_); \
+	if (_cmp_ >= 0) { _bs_->low = _bs_->mid; _bs_->equal = _cmp_ == 0; } \
 	else { _bs_->high = _bs_->mid - 1; } \
 } while(0)
 
-#define BSEARCH_STEP_SEARCH_LAST_LT(bs_, cmp_) do { \
+#define BSEARCH_STEP_TO_LAST_LT(bs_, cmp_) do { \
 	__typeof__(bs_) _bs_ = (bs_); \
-	__typeof__(cmp_) cmp = (cmp); \
-	if (cmp > 0) { _bs_->low = _bs_->mid; } \
+	__typeof__(cmp_) _cmp_ = (cmp_); \
+	if (_cmp_ > 0) { _bs_->low = _bs_->mid; } \
 	else { _bs_->high = _bs_->mid - 1; } \
 } while(0)
 
 #define BSEARCH_INIT_SEARCH_FIRST(bs_, count) do { \
 	__typeof__(bs_) _bs_ = (bs_); _bs_->low = _bs_->mid = 0; _bs_->high = (count); _bs_->equal = 0; \
+	assert(_bs_->low <= _bs_->high); \
 } while(0)
 
-#define BSEARCH_NEXT_SEARCH_FIRST(bs_) ({ \
+#define BSEARCH_FIRST_NOT_FOUND(bs_) ({ \
 	__typeof__(bs_) _bs_ = (bs_); \
 	_bs_->mid = _bs_->low + ((_bs_->high - _bs_->low) / 2); \
 	_bs_->low < _bs_->high; \
 })
 
-#define BSEARCH_STEP_SEARCH_FIRST_GE(bs_, cmp_) do { \
+#define BSEARCH_STEP_TO_FIRST_GE(bs_, cmp_) do { \
 	__typeof__(bs_) _bs_ = (bs_); \
-	__typeof__(cmp_) cmp = (cmp); \
-	if (cmp <= 0) { _bs_->high = _bs_->mid; _bs_->equal = cmp == 0; } \
+	__typeof__(cmp_) _cmp_ = (cmp_); \
+	if (_cmp_ <= 0) { _bs_->high = _bs_->mid; _bs_->equal = _cmp_ == 0; } \
 	else { _bs_->low = _bs_->mid + 1; } \
 } while(0)
 
-#define BSEARCH_STEP_SEARCH_FIRST_GT(bs_, cmp_) do { \
+#define BSEARCH_STEP_TO_FIRST_GT(bs_, cmp_) do { \
 	__typeof__(bs_) _bs_ = (bs_); \
-	__typeof__(cmp_) cmp = (cmp); \
-	if (cmp < 0) { _bs_->high = _bs_->mid; } \
+	__typeof__(cmp_) _cmp_ = (cmp_); \
+	if (_cmp_ < 0) { _bs_->high = _bs_->mid; } \
 	else { _bs_->low = _bs_->mid + 1; } \
 } while(0)
 
 #define BSEARCH_INIT_SEARCH_EQUAL(bs_, count) BSEARCH_INIT_SEARCH_FIRST((bs_), (count))
-#define BSEARCH_NEXT_SEARCH_EQUAL(bs_) BSEARCH_NEXT_SEARCH_FIRST(bs_)
-#define BSEARCH_STEP_SEARCH_EQUAL(bs_, cmp_) do { \
+#define BSEARCH_EQUAL_NOT_FOUND(bs_) BSEARCH_FIRST_NOT_FOUND(bs_)
+#define BSEARCH_STEP_TO_EQUAL(bs_, cmp_) do { \
 	__typeof__(bs_) _bs_ = (bs_); \
-	__typeof__(cmp_) cmp = (cmp); \
-	if (cmp < 0) { _bs_->high = _bs_->mid; } \
-	else if (cmp > 0) { _bs_->high = _bs_->mid - 1; } \
+	__typeof__(cmp_) _cmp_ = (cmp_); \
+	if (_cmp_ < 0) { _bs_->high = _bs_->mid; } \
+	else if (_cmp_ > 0) { _bs_->low = _bs_->mid + 1; } \
 	else { _bs_->low = _bs_->high = _bs_->mid; _bs_->equal = 1; } \
 } while(0)
 
 /*
+	// if int indices are just enough
 	bs_t bs;
 	bs_init_search_last(&bs, count);
-	while (bs_next_search_last(&bs)) {
-		bs_step_search_last_le(&bs, cmp(key, objs[bs.mid]));
+	while (bs_last_not_found(&bs)) {
+		bs_step_to_last_le(&bs, cmp(key, objs[bs.mid]));
 	}
+	print("index of last less or equal %d equal %d", bs.mid, bs.equal);
 
 	bs_init_search_first(&bs, count);
-	while (bs_next_search_first(&bs)) {
-		bs_step_search_first_gt(&bs, cmp(key, objs[bs.mid]));
+	while (bs_first_not_found(&bs)) {
+		bs_step_to_first_gt(&bs, cmp(key, objs[bs.mid]));
 	}
+	print("index of first greater %d", bs.mid);
+
+	bs_init_search_equal(&bs, count);
+	while (bs_equal_not_found(&bs)) {
+		bs_step_to_equal(&bs, cmp(key, objs[bs.mid]));
+	}
+	print("index of some equal or first greater %d equal %d", bs.mid, bs.equal);
 */
 typedef BSEARCH_STRUCT(int) bs_t;
 static inline void bs_init_search_last(bs_t *bs, int count) { BSEARCH_INIT_SEARCH_LAST(bs, count); }
 static inline void bs_init_search_first(bs_t *bs, int count) { BSEARCH_INIT_SEARCH_FIRST(bs, count); }
 static inline void bs_init_search_equal(bs_t *bs, int count) { BSEARCH_INIT_SEARCH_EQUAL(bs, count); }
-static inline int  bs_next_search_last(bs_t *bs) { return BSEARCH_NEXT_SEARCH_LAST(bs); }
-static inline int  bs_next_search_first(bs_t *bs) { return BSEARCH_NEXT_SEARCH_FIRST(bs); }
-static inline int  bs_next_search_equal(bs_t *bs) { return BSEARCH_NEXT_SEARCH_EQUAL(bs); }
-static inline void bs_step_search_last_le(bs_t *bs, int64_t cmp) { BSEARCH_STEP_SEARCH_LAST_LE(bs, cmp); }
-static inline void bs_step_search_last_lt(bs_t *bs, int64_t cmp) { BSEARCH_STEP_SEARCH_LAST_LT(bs, cmp); }
-static inline void bs_step_search_first_ge(bs_t *bs, int64_t cmp) { BSEARCH_STEP_SEARCH_FIRST_GE(bs, cmp); }
-static inline void bs_step_search_first_gt(bs_t *bs, int64_t cmp) { BSEARCH_STEP_SEARCH_FIRST_GT(bs, cmp); }
-static inline void bs_step_search_equal(bs_t *bs, int64_t cmp) { BSEARCH_STEP_SEARCH_EQUAL(bs, cmp); }
+static inline int  bs_last_not_found(bs_t *bs) { return BSEARCH_LAST_NOT_FOUND(bs); }
+static inline int  bs_first_not_found(bs_t *bs) { return BSEARCH_FIRST_NOT_FOUND(bs); }
+static inline int  bs_equal_not_found(bs_t *bs) { return BSEARCH_EQUAL_NOT_FOUND(bs); }
+static inline void bs_step_to_last_le(bs_t *bs, int64_t cmp) { BSEARCH_STEP_TO_LAST_LE(bs, cmp); }
+static inline void bs_step_to_last_lt(bs_t *bs, int64_t cmp) { BSEARCH_STEP_TO_LAST_LT(bs, cmp); }
+static inline void bs_step_to_first_ge(bs_t *bs, int64_t cmp) { BSEARCH_STEP_TO_FIRST_GE(bs, cmp); }
+static inline void bs_step_to_first_gt(bs_t *bs, int64_t cmp) { BSEARCH_STEP_TO_FIRST_GT(bs, cmp); }
+static inline void bs_step_to_equal(bs_t *bs, int64_t cmp) { BSEARCH_STEP_TO_EQUAL(bs, cmp); }
 
 #endif
