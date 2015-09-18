221eae69b356b28fae6c9892b9a625095fb5062e
Yuriy Vostrikov <vostrikov@corp.mail.ru>
2015-09-18 10:00:10 +0300

    Add missing logging

--- src/log_io_puller.m
+++ src/log_io_puller.m
@@ -183,6 +183,7 @@ establish_connection
 	say_debug2("%s: connect", __func__);
 	if ((fd = tcp_connect(&feeder->addr, NULL, 5)) < 0) {
 		snprintf(errbuf, sizeof(errbuf), "can't connect, %s", strerror_o(errno));
+		say_debug(errbuf);
 		return -1;
 	}
 
@@ -320,7 +321,7 @@ handshake:(i64)scn
 	}
 
 	if (version != default_version && version != version_11) {
-		snprintf(errbuf, sizeof(errbuf), "unknown remote version");
+		snprintf(errbuf, sizeof(errbuf), "unknown remote version (%i)", version);
 		goto err;
 	}
 
@@ -328,6 +329,7 @@ handshake:(i64)scn
 	say_info("starting remote recovery from scn:%" PRIi64, scn);
 	return 1;
 err:
+	say_error(errbuf);
 	tbuf_reset(&rbuf);
 	if (fd >= 0) {
 		close(fd);
