675878d6fc338012416845418708584512c53368
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2016-09-12 17:16:52 +0300

    24_test_index_64bit: remove usage of facets

--- test/24_test_index_64bit.rb
+++ test/24_test_index_64bit.rb
@@ -3,7 +3,6 @@
 
 $: << File.dirname($0) + '/lib'
 require 'run_env'
-require 'facets'
 
 class Env < RunEnv
   def config
@@ -14,7 +13,7 @@ EOD
 
   task :setup => "box_init.lua"
   file "box_init.lua" do
-    File.create "box_init.lua", <<-EOD
+    File.write "box_init.lua", <<-EOD
         local box = require'box'
         user_proc.select = box.wrap(function (shard, n)
                 return 0, {shard:object_space(n):index(0):find(0ULL),
