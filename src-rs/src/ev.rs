a0af27b4dc743148f9ae95c17f72f30d971e01e0
Yury Vostrikov <mon@unformed.ru>
2020-08-09 21:05:34 +0200

    Add ev::Stat event handler

--- src-rs/src/ev.rs
+++ src-rs/src/ev.rs
@@ -23,7 +23,80 @@
  * SUCH DAMAGE.
  */
 
+#![allow(dead_code)]
+
+use paste::paste;
+use std::ffi::CString;
+use std::path::Path;
+use std::os::unix::ffi::OsStrExt;
+
 mod ffi {
     #![allow(dead_code, non_camel_case_types)]
     include!("octopus_ev.rs");
+}
+
+macro_rules! ev_common_methods {
+    ($typ:ident) => {
+        // TODO: full zeriong is not required, only "public" fields must be initialized
+        // libev provides ev_$typ_init() initializaion macro, which only inits required feilds
+        // it's possible to mimic this behaviour with std::mem::UNINIT
+        pub fn new() -> Self {
+            unsafe { std::mem::zeroed() }
+        }
+
+        pub fn is_pending(&self) -> bool {
+            self.active != 0
+        }
+
+        pub fn is_active(&self) -> bool {
+            self.active != 0
+        }
+
+        pub fn priority(&self) -> i32 {
+            self.priority
+        }
+
+        paste! {
+            // FIXME: self must be pined
+            //
+            // self must be inited before calling start()
+            //
+            // it's possible to check for initialization by ensuring
+            // that cb is Some and panic otherwise; however calling
+            // NULL cb will certainly segfault and that is as
+            // good as panic()
+            pub fn start(&mut self) {
+                unsafe { ffi::[<ev_ $typ _start>](self) }
+            }
+
+            pub fn stop(&mut self) {
+                unsafe { ffi::[<ev_ $typ _stop>](self) }
+            }
+        }
+    }
+}
+
+pub type TStamp = ffi::ev_tstamp;
+pub type Stat = ffi::ev_stat;
+
+impl Stat {
+    ev_common_methods!(stat);
+
+    pub fn init(&mut self, cb: extern "C" fn(*mut Self, libc::c_int), path: &Path, interval: TStamp) {
+        self.cb = Some(cb);
+        if !self.path.is_null() {
+            drop(unsafe { CString::from_raw(self.path as *mut _) });
+        }
+        self.path = CString::new(path.as_os_str().as_bytes()).unwrap().into_raw();
+        self.interval = interval;
+    }
+}
+
+impl Drop for Stat {
+    fn drop(&mut self) {
+        self.stop();
+        if !self.path.is_null() {
+            drop(unsafe { CString::from_raw(self.path as *mut _) });
+        }
+    }
 }
